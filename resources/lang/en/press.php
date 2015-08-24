478a456cc678c8496842191ccc8e2291ec7e72bf
Karoly Szabo <karoly.szabo@intesys.it>
2015-08-24 11:57:05 +0200

    Translations and template

--- resources/lang/en/press.php
+++ resources/lang/en/press.php
@@ -12,6 +12,43 @@ return [
     |
     */
 
-    'register' => 'Register',
 
+    'register' => 'Registration',
+    'contact' => [
+        'head' => 'Contact information',
+        'name' => 'Name',
+        'surname' => 'Surname',
+        'email' => 'Personal E-mail where the press pass and all Veronafiere press communications will be sent',
+        'cardnr' => 'Union of Journalists card nr.',
+        'mobile' => 'Mobile phone',
+        'qualification' => 'Qualification ',
+        'workfor' => 'Work for',
+        'twitter' => 'Twitter Account',
+    ],
+    'infonewspaper' => 'Information about publication(s) represented',
+    'newspaper' => 'Publication represented',
+    'address' => 'Street',
+    'city' => 'City',
+    'zipcode' => 'ZIP code',
+    'nationality' => 'Nationality',
+    'schedule' => 'Publication schedule',
+    'emailpublisher' => 'Publisher\'s email',
+    'phone' => 'Landline',
+    'website' => 'Web Site',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'firsttimevisiting' => 'Is this the first time you will visit the event?',
+    'infouploadhere' => 'Upload the documents requested here in PDF, JPG or DOC format.<br>Total size of attachment must not exceed 3 megabytes (3Mb).',
+    'uploadletter' => 'Copy of your engagement letter from your publisher',
+    'uploadcolophon' => 'copy of articles published with your signature in printed magazines dealing with the event',
+    'uploadarticle' => 'Copy of a document testifying to journalistic activity',
+    'infocommunications' => 'Your Communications',
+    'infoprivacy' => 'Privacy Notification',
+    'privacycheckbox' => 'I agree to processing of my personal data in conformity with the standards of <a href="{0}">privacy laws</a>.',
+    'sendrequest' => 'Send accreditation request',
+    'sendrequestdescription' => 'A confirmation screen will notify the successful outcome of sending the accreditation request. Any errors in the form compilation procedure are highlighted in red.',
+    'sendrequestnotice' => 'Before sending the request make sure you have filled in all the required fields (marked with asterisks)',
+
+    'submit' => 'Send accreditation request for :fair_name',
+    'reset' => 'Reset'
 ];
--- resources/lang/it/press.php
+++ resources/lang/it/press.php
@@ -13,5 +13,42 @@ return [
     */
 
     'register' => 'Registrazione',
+    'contact' => [
+        'head' => 'Informazioni di contatto',
+        'name' => 'Nome',
+        'surname' => 'Cognome',
+        'email' => 'Indirizzo E-mail',
+        'cardnr' => 'Nr. tessera dell\'Unione de Giornalisti',
+        'mobile' => 'Cellulare',
+        'qualification' => 'Qualifica',
+        'workfor' => 'Lavori per',
+        'twitter' => 'Account Twitter',
+    ],
+    'infonewspaper' => 'Informazioni relative alla testata e/o testate rappresentate',
+    'newspaper' => 'Testata e/o testate rappresentate',
+    'address' => 'Indirizzo',
+    'city' => 'Citt&agrave;',
+    'zipcode' => 'CAP',
+    'nationality' => 'Nazionalit&agrave;',
+    'schedule' => 'Periodicit&agrave;',
+    'emailpublisher' => 'E-mail redazione',
+    'phone' => 'Telefono fisso',
+    'website' => 'sito web',
+    'yes' => 'S&igrave;',
+    'no' => 'No',
+    'firsttimevisiting' => '&Egrave; la prima volta che visiti la manifestazione?',
+    'infouploadhere' => 'Esegui qui l\'upload dei documenti richiesti, in formato PDF, JPG o DOC.<br>Il totale delle dimensioni degli allegati non può superare i 3 megabyte (3Mb).',
+    'uploadletter' => 'Copia della lettera d\'incarico della testata/radio/tv/sito web registrato per cui si collabora, firmata dal direttore o caporedattore',
+    'uploadcolophon' => 'Copia del colophon o delle indicazioni sulla gerenza della testata/radio/tv/sito web registrato',
+    'uploadarticle' => 'Copia di un articolo recente pubblicato a propria firma su testate cartacee, oppure link ad un servizio audio/video o ad un articolo pubblicato su testate online registrate, che testimoni l\'effettiva attività giornalistica svolta nel settore inerente alla manifestazione per cui si richiede l\'accredito. Per chi ha gi&agrave; partecipato alla precedente edizione della manifestazione, il materiale allegato deve testimoniarne l\'avvenuta copertura',
+    'infocommunications' => 'Vostre comunicazioni',
+    'infoprivacy' => 'Informativa sulla privacy',
+    'privacycheckbox' => 'Letta l\'<a href="{0}">informativa sulla privacy</a> ai sensi del Dlg 196/03, acconsento al trattamento dei dati personali per le finalit&agrave; e con le modalit&agrave; specificatamente indicate nell\'informativa stessa',
+    'sendrequest' => 'Invia richiesta accredito',
+    'sendrequestdescription' => 'Una schermata di conferma testimonia il buon esito dell\'invio della richiesta di accredito. Eventuali errori nella procedura di compilazione del form sono evidenziati in rosso.',
+    'sendrequestnotice' => 'Prima di inviare la richiesta assicurati di aver compilato tutti i campi obbligatori (contrassegnati da asterisco)',
 
-];
+    'submit' => 'Invia richiesta per :fair_name',
+    'reset' => 'Cancella'
+
+    ];
