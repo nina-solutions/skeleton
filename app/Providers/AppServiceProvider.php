12f6aaa00a0bc9a766470ce2bfc6d567e5f253c2
Karoly Szabo <karoly.szabo@intesys.it>
2015-08-31 18:15:35 +0200

    query analisys if local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace FairHub\Providers;
 
+use Illuminate\Contracts\Logging\Log;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function($sql, $bindings, $time) {
+            if('local' == env('APP_ENV')) {
+                echo $sql . PHP_EOL;
+                print_r($bindings);
+                echo $time . PHP_EOL;
+                echo PHP_EOL;
+            }
+        });
     }
 
     /**
