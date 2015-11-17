5ba84eac97d6788058ce54e12808d3daaf8e8930
Karoly Szabo <karoly.szabo@intesys.it>
2015-11-17 15:44:11 +0100

    get virtual attributes

--- app/Models/HubContent.php
+++ app/Models/HubContent.php
@@ -6,12 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class HubContent extends HubModel
 {
+    public function getContextNameAttribute(){
+        $context = $this->content->context()->first();
+        if ($context !== null)
+            return $context->name;
+        return null;
+    }
+
+    public function getContentNameAttribute(){
+        $content = $this->content();
+        dd($content);
+        if ($content !== null)
+            return $content->name;
+        return null;
+    }
 
     /**
      * Get all of the product's photos.
      */
-    public function contents()
+    public function content()
     {
-        return $this->morphMany('FairHub\Models\Content', 'contentable');
+        return $this->morphOne('FairHub\Models\Content', 'contentable');
     }
 }
