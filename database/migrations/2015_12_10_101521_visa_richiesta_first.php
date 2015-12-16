7a41227cab2c4b1ef2e2f9b810e558e68bd5ce23
Karoly Albert Szabo <karoly.szabo@intesys.it>
2015-12-16 12:28:44 +0100

    Fix double declaration of same class name

--- database/migrations/2015_12_10_101521_visa_richiesta_first.php
+++ database/migrations/2015_12_10_101521_visa_richiesta_first.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VisaRichiesta extends Migration
+class VisaRichiestaFirst extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2015_12_11_101521_visa_richiesta_second.php
+++ database/migrations/2015_12_11_101521_visa_richiesta_second.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VisaRichiesta extends Migration
+class VisaRichiestaSecond extends Migration
 {
     /**
      * Run the migrations.
