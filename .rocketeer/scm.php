2688dd7fc4fd39e68e56c3d68aa1c06fd3f2fd29
Karoly Albert Szabo <karoly.szabo@intesys.it>
2015-12-16 12:00:56 +0100

    Ask for credentials before clone the repo

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -17,8 +17,8 @@ return [
     // In other cases you can leave this empty too, and you will
     // be prompted for the credentials on deploy. If you don't want
     // to be prompted (public repo, etc) set the values to null
-    'username'   => 'kszabo',
-    'password'   => '***',
+    'username'   => '',
+    'password'   => '',
 
     // The branch to deploy
     'branch'     => 'develop',
